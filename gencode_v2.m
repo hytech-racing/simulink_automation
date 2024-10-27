ff8fa7b44301387ebd115cac02d06746f6eac3ab
Krish Kittur <kittur.krish@gmail.com>
2024-10-26 21:44:58 -0400

    Rename model list to matlab_math

--- gencode_v2.m
+++ gencode_v2.m
@@ -4,7 +4,7 @@ addpath(genpath(rootFolder));  % Add all sub-folders
 
 HT09_Vehicle_Parameters;
 
-modelList = {'Tire_Model_Codegen'}; 
+modelList = {'matlab_math'}; 
 close all;
 
 % Cell array to store the generated zip file names
